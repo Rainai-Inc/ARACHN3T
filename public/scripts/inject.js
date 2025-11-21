43881a5a2581d3d799442e26655d058a487a68a5
Itachi Senpai <72074802+Rainai-Inc@users.noreply.github.com>
2025-11-21 07:04:39 -0500

    Delete public/scripts/inject.js

--- public/scripts/inject.js
+++
@@ -1,8 +0,0 @@
-const currFrontState = document.getElementById('memoryLink0');
-let newFrontState = currFrontState.replaceAll(' ','');
-let passedFrontState = newFrontState.split('\n');
-passedFrontState.forEach(item => {
-    if (item.length !== 0) {
-        let newItem = item.replaceAll('PAID-SERVICE','<svg src=alert(1)>');
-    }
-});
