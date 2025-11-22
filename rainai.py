71ca9817921f28cb5046fbf9f1318e6487fb17f6
Itachi Senpai <72074802+Rainai-Inc@users.noreply.github.com>
2025-11-22 08:58:19 -0500

    Clean up rainai.py by removing unused comments
    Removed commented-out code and documentation for clarity.

--- rainai.py
+++ rainai.py
@@ -64,13 +64,6 @@ def think(h) -> float|int:
         else:
             return False,(-1,-1)
 
-    # for i in range(random(len(h))):
-    #   if h{i} in k:
-    #       response+=0.01
-    # if response<-0.45:
-    #   Respective certainty.
-    #   speak(response)
-
 class mya:
     def __init__(self,human,state=nState()):
         self.human=human
@@ -79,19 +72,6 @@ class mya:
         self.stateCommit=-0.15
         self.stateAware=stateZero
         self.workReq=open('workReqTemp','a+')
-        #
-        # Security Model
-        #
-        #
-        # Where certainty is met -> Awareness is held though bias is undetermined
-        # Allow transparency by informing users this information is applicable for
-        # learning, though, not accepted for security reasons. (Web app)
-        # --->
-        # Meeting compliance safety standards based off user choice.
-        # Rainai Inc. has the right to refuse human prompts that do not align with out moral,
-        # sociological, and economical beliefs. And will not be held responsible for any
-        # activity or behavior derived from customer training choices. (Mobile App)
-        #
     def knowSelf(self):
         # self is either aware of knowing of given (h) or knowingly aware of (h) certain
         # value
@@ -171,29 +151,3 @@ class mya:
                         print(f"Error: {e}")
             except:
                 return False
-
-
-    # StateZero = 0
-    # Setting applicable range by tens or hundredths based
-    # based off sum.
-    # In example 100.00 --> 50.00 --> 25.00 --> 5.00 --> 0.75
-    # If h is valid increase from stateZero with all given
-    # parameters where 1=100.
-    # Base logic for algotithmically applying precision and accuracy scoring.
-    # Said, False values reduce and are capable of reaching a negative score of StateOne itself.
-    #
-    # Lets say -1=-100.00
-    # Current Tasks:
-    # Default state for model is 0 or duality in a sense.
-    # Knowing itself (definition in progress )and current state yet uknowing to fact h(s)needs.
-    # The embedding.
-
-    # After embedding and integration. Analysis summary should define purposes.
-    #
-    # Based on occupation, intuitively provide assistance that saves time.
-    #
-    # Example: Summary = Content Creator
-    #
-    # {"NTAM": {"Occupation":"Content-Creator,Director,Photographer,Streamer", "Action":"Sentify", "Token": "2c57d4b0297f7c7741ce8c738e0d54db8e925ec2a7c58e584e8513c014f9abef67aee2cac77bfbb78ddee6fe2572d3b17d9e89007d9a7442a48d4ed17024ddc6"}}
-    #
-    # mya(validate("Example message from authorized"), (validate(stateZero(-1))).
